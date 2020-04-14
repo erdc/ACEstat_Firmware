eef9c20c3edd3f9fc6caacdec429f7716d1f3108
Eric Brown <eric.w.brown@erdc.dren.mil>
2020-04-14 14:58:14 -0500

    reflects most recent changes to voltammetry.c functions

--- erdcWorkSpace/erdcTemplate/erdc_main/chemcrab_lib/voltammetry.h
+++ erdcWorkSpace/erdcTemplate/erdc_main/chemcrab_lib/voltammetry.h
@@ -20,16 +20,19 @@
 /*Imax = 0.9V/RGAIN*/
 #define mvStepDelay 147 //delay for 5mV/S
 
+//Retrieves voltammetry parameters from UART input
+uint16_t getParameter(int dec);
+
 //Cyclic Voltammetry Functions
-void runCV(uint8_t* szInSring);
-void cv_ramp_parameters(uint16_t start, uint16_t mid, uint16_t end, uint32_t RGAIN);
-void printCVResults(float cStart, float cMid, float cEnd, int samplesCount, int RTIA);
+void runCV(void);
+void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t midV, uint16_t endV, uint32_t RGAIN);
+void printCVResults(float cZero, float cStart, float cMid, float cEnd, int samplesCount, int RTIA);
 
 //Square-wave Voltammetry Functions
-void runSWV(uint8_t* szInSring);
+void runSWV(void);
 void sqv_dep_time(uint16_t start, uint16_t time);
-void sqv_ramp_parameters(uint16_t start, uint16_t end, uint32_t RGAIN, uint16_t amplitude, int dep);
-void printSWVResults(float cStart, float cEnd, uint16_t amp, int sampleCount, int RTIA, int dep);
+void sqv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, int dep);
+void printSWVResults(float cZero, float cStart, float cEnd, uint16_t amp, int sampleCount, int RTIA, int dep);
 
 
 #endif
