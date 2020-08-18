d8cc7df269d53d4c05778bc1867bf425d0cb7ce7
Eric Brown <eric.w.brown@erdc.dren.mil>
2020-08-18 10:00:34 -0500

    Returning to fixed VZERO, now directly measuring sensor voltage

--- erdcWorkSpace/src/chemcrab_lib/voltammetry.h
+++ erdcWorkSpace/src/chemcrab_lib/voltammetry.h
@@ -19,14 +19,13 @@
 
 //Cyclic Voltammetry Functions
 void runCV(void);
-void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t midV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate, uint16_t res);
-void printCVResults(float cZero, float cStart, float cMid, float cEnd, int samplesCount, int RTIA);
+void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t vertexV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate, uint16_t burstSamples);
+void printCVResults(float cZero, float cStart, float cVertex, float cEnd, int sampleCount, int RTIA);
 
 //Square-wave Voltammetry Functions
 void runSWV(void);
 void sqv_dep_time(uint16_t start, uint16_t time);
-void sqv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, int dep);
-void printSWVResults(float cZero, float cStart, float cEnd, uint16_t amp, int sampleCount, int RTIA, int dep);
-
+void sqv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, int dep, uint16_t freq);
+void printSWVResults(float cZero, float cStart, float cEnd, uint16_t amp, int sampleCount, int RTIA, int dep, int freq);
 
 #endif
