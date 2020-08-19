56bf1a55e96333711a376c5c77034d1a461c1c6f
Eric Brown <eric.w.brown@erdc.dren.mil>
2020-08-19 16:13:22 -0500

    added burst sampling and sensor voltage measurements to squarewave voltammetry

--- erdcWorkSpace/src/chemcrab_lib/voltammetry.h
+++ erdcWorkSpace/src/chemcrab_lib/voltammetry.h
@@ -19,12 +19,12 @@
 
 //Cyclic Voltammetry Functions
 void runCV(void);
-void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t vertexV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate, uint16_t burstSamples);
+void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t vertexV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate);
 void printCVResults(float cZero, float cStart, float cVertex, float cEnd, int sampleCount, int RTIA);
 
 //Square-wave Voltammetry Functions
 void runSWV(void);
-void sqv_dep_time(uint16_t start, uint16_t time);
+//void sqv_dep_time(uint16_t start, uint16_t time);
 void sqv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, int dep, uint16_t freq);
 void printSWVResults(float cZero, float cStart, float cEnd, uint16_t amp, int sampleCount, int RTIA, int dep, int freq);
 
