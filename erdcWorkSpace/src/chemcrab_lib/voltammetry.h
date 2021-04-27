992701c1634adcd9473fae784f5bc71f4c36fa44
Eric Brown <eric.w.brown@erdc.dren.mil>
2021-04-27 20:27:12 +0000

    -Added support for programmable LPTIA gain resistor with correct current calculation adjustment -Added sensor channel as selectable parameter for all test modes

--- erdcWorkSpace/src/chemcrab_lib/voltammetry.h
+++ erdcWorkSpace/src/chemcrab_lib/voltammetry.h
@@ -19,13 +19,15 @@
 
 //Cyclic Voltammetry Functions
 void runCV(void);
-void cv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t vertexV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate);
+void cv_ramp_parameters(uint8_t chan, uint16_t zeroV, uint16_t startV, uint16_t vertexV, uint16_t endV, uint32_t RGAIN, uint16_t sweepRate);
 void printCVResults(float cZero, float cStart, float cVertex, float cEnd, int sampleCount, int RTIA);
 
 //Square-wave Voltammetry Functions
 void runSWV(void);
-void equilibrium_delay(uint16_t start, uint16_t zero , uint16_t time);
-void sqv_ramp_parameters(uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, uint16_t step, uint16_t freq);
+void sqv_ramp_parameters(uint8_t chan, uint16_t zeroV, uint16_t startV, uint16_t endV, uint32_t RGAIN, uint16_t amplitude, uint16_t step, uint16_t freq);
 void printSWVResults(float cZero, float cStart, float cEnd, int sampleCount, int RTIA);
+
+//Shared Functions
+void equilibrium_delay(uint8_t chan, uint16_t start, uint16_t zero , uint16_t time);
 
 #endif
