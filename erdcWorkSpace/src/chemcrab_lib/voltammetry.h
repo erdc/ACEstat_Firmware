99b23791f58754db1308ca7731c90da1a6d64a01
Eric Brown <eric.w.brown@erdc.dren.mil>
2021-07-01 16:53:15 +0000

    Added CSWV(cyclic square wave voltammetry) implementation and supporting functions

--- erdcWorkSpace/src/chemcrab_lib/voltammetry.h
+++ erdcWorkSpace/src/chemcrab_lib/voltammetry.h
@@ -17,18 +17,25 @@
 
 #include "craabUtil.h"
 
-//Cyclic Voltammetry Functions
+//Cyclic Voltammetry(CV) Functions
 void runCV(void);
 void set_CV_voltages(int relative_voltages[3], uint16_t absolute_voltages[4]);
 void cv_ramp_parameters(uint16_t chan, int startV, int vertexV, int endV, uint32_t RGAIN, uint16_t sweepRate);
 void printCVResults(float cZero, float cStart, float cVertex, float cEnd, int sampleCount, int RTIA);
 void equilibrium_delay_CV(uint16_t chan, int startV, int vertexV, int endV, uint16_t time);
 
-//Square-wave Voltammetry Functions
+//Square-wave Voltammetry(SWV) Functions
 void runSWV(void);
 void set_SWV_voltages(int relative_voltages[3], uint16_t absolute_voltages[3]);
-void sqv_ramp_parameters(uint16_t chan, int startV, int endV, uint32_t RGAIN, uint16_t amplitude, uint16_t step, uint16_t freq);
+void swv_ramp_parameters(uint16_t chan, int startV, int endV, uint32_t RGAIN, uint16_t amplitude, uint16_t step, uint16_t freq);
 void printSWVResults(float cZero, float cStart, float cEnd, int sampleCount, int RTIA);
 void equilibrium_delay_SWV(uint16_t chan, int startV, int endV, uint16_t amp, uint16_t time);
+
+//Cyclic Square-wave Voltammetry(CSWV) Functions
+void runCSWV(void);
+void set_CSWV_voltages(int relative_voltages[4], uint16_t absolute_voltages[4]);
+void cswv_ramp_parameters(uint16_t chan, int startV, int vertexV, int endV, uint32_t RGAIN, uint16_t amplitude, uint16_t step, uint16_t freq);
+void printCSWVResults(float cZero, float cStart, float cVertex, float cEnd, int sampleCount, int RTIA);
+void equilibrium_delay_CSWV(uint16_t chan, int startV, int vertexV, int endV, uint16_t amp, uint16_t time);
 
 #endif
